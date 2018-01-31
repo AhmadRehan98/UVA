cb93de55f22979c8a7a444550b99b2908d3179be
Ahmed Rehan <ahmedrehan98@gmail.com>
2018-01-31 15:07:16 +0200

    Create 11636 - Hello World!.cpp

---
+++ 11636 - Hello World!.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, cases=1;
+    while (cin >> n && n>0) {
+        cout << "Case " << cases << ": " << ceil(log2(n)) << endl;
+        cases++;
+    }
+}
